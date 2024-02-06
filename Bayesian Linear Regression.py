a3b144e3f5dbe866a9f15f3d11e42e45e59ab02d
Nathaniel Cogneaux <Nathaniel@cogneaux.net>
2024-02-06 12:30:27 +0100

    Commit

---
+++ Bayesian Linear Regression.py
@@ -0,0 +1,7 @@
+# Work with Bayesian Linear Regression models with varying basis functions (linear, polynomial and Gaussian).
+# Datasets used are 1D toy regression samples ranging from linear datasets
+# to more complex non-linear datasets such as increasing sinusoidal curves.
+
+# import libs
+import numpy as np
+import matplotlib.pyplot as plt
